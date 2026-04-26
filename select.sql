168b748def41224edc5e41c00f080aad0bbbcdbd
akshata_yatnal <yatnalakshata@gmail.com>
2026-04-26 09:08:21 +0530

    Add files via upload

---
+++ select.sql
@@ -0,0 +1,39 @@
+-- Q1: Display all details from the employee table
+SELECT * 
+FROM emp;
+
+-- Q2: Display employee name, salary and commission
+SELECT ename, sal, comm 
+FROM emp;
+
+-- Q3: Display names and salaries of all employees
+SELECT ename, sal
+ FROM emp;
+
+-- Q4: Display names and commission of all employees
+SELECT ename, comm 
+FROM emp;
+
+-- Q5: Display employee ID and department number
+SELECT empno, deptno 
+FROM emp;
+
+-- Q6: Display employee name and hire date
+SELECT ename, hiredate 
+FROM emp;
+
+-- Q7: Display employee name and designation (job)
+SELECT ename, job 
+FROM emp;
+
+-- Q8: Display employee name, job and salary
+SELECT ename, job, sal 
+FROM emp;
+
+-- Q9: Display department names from department table
+SELECT dname 
+FROM dept;
+
+-- Q10: Display department name and location
+SELECT dname, loc 
+FROM dept;
