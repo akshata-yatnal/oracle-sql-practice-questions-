a09a58029a90dc480c276dd8a4ee77f10b87fa81
akshata_yatnal <yatnalakshata@gmail.com>
2026-04-30 15:06:09 +0530

    Add files via upload

---
+++ order by.sql
@@ -0,0 +1,20 @@
+-- Q21: Display all employees ordered by salary.
+        SELECT *
+        FROM EMP
+        ORDER BY SAL;
+-- Q22: Display employee names ordered alphabetically.
+        SELECT ENAME
+        FROM EMP
+        ORDER BY ENAME;
+-- Q23: Display employees ordered by hire date.
+        SELECT * 
+        FROM EMP
+        ORDER BY HIREDATE; 
+-- Q24: Display employees ordered by salary in descending order.
+        SELECT *
+        FROM EMP
+        ORDER BY SAL DESC;
+-- Q25: Display employees ordered by department number.
+        SELECT *
+        FROM EMP
+        ORDER BY DEPTNO;
